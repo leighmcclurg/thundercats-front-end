63e2c9b20faa535219720128e215ec91c6fa66c1
MylesScullion <myles.scullion@kainos.com>
2023-08-31 12:05:27 +0100

    Creating controller, service, and salesemployee

---
+++ model/salesEmployee.ts
@@ -0,0 +1,8 @@
+export class SalesEmployee {
+    salesEmployeeID?: number
+    name?: string
+    salary?: number
+    bankAccountNumber?: string
+    nationalInsuranceNumber?: string
+    commissionRate?: number
+}
